78f165c00e4fd7f4842c208a120e48963e55a6aa
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-06-16 15:06:23 -0300

    Update Perfil.py

--- Perfil.py
+++ Perfil.py
@@ -10,6 +10,7 @@ from funcoes_crud import buscar_registro, atualizar_registro, deletar_registro
 conexao_e_cabecalho()
 
 usuario_auth = st.session_state.get("usuario")
+
 if not usuario_auth:
     st.warning("Você precisa estar logado para acessar esta página.")
     st.stop()
@@ -21,53 +22,3 @@ email = usuario_auth.get("email")
 usuarios = buscar_registro(nome_tabela="usuarios", ordenar_por="id", colunas=["id", "nome", "email", "senha"])
 
 usuario = next((u for u in usuarios if u["email"] == email), None)
-
-'''
-if not usuario:
-    st.error("Usuário não encontrado no banco.")
-    st.stop()
-
-usuario_id = usuario["id"]
-nome_atual = usuario["nome_usuario"]
-cargo = usuario.get("cargo", "Não especificado")
-
-
-# --- Layout ---
-
-st.subheader("Editar Informações")
-novo_nome = st.text_input("Nome", value=nome_atual)
-st.text_input("Email", value=email, disabled=True)
-st.text_input("Cargo", value=cargo, disabled=True)
-
-with st.expander("🔐 Alterar senha"):
-    nova_senha = st.text_input("Nova senha", type="password")
-    confirmar_senha = st.text_input("Confirmar nova senha", type="password")
-
-if st.button("💾 Salvar alterações"):
-    atualizacoes = {"nome_usuario": novo_nome}
-    if nova_senha:
-        if nova_senha != confirmar_senha:
-            st.error("As senhas não coincidem.")
-            st.stop()
-        nova_senha_hash = bcrypt.hashpw(nova_senha.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
-        atualizacoes["senha"] = nova_senha_hash
-
-    resultado = atualizar_registro("usuarios", usuario_id, atualizacoes)
-    if resultado:
-        st.success("Perfil atualizado com sucesso!")
-
-st.markdown("---")
-st.subheader("⚠️ Excluir Conta")
-
-with st.expander("🗑️ Deseja excluir sua conta permanentemente?"):
-    confirmacao = st.text_input("Digite CONFIRMAR para excluir sua conta:")
-    if st.button("Excluir conta", type="primary"):
-        if confirmacao == "CONFIRMAR":
-            deletar = deletar_registro("usuarios", usuario_id)
-            if deletar:
-                st.success("Conta excluída com sucesso.")
-                st.session_state.clear()
-                st.experimental_rerun()
-        else:
-            st.error("Confirmação incorreta. Digite exatamente CONFIRMAR para prosseguir.")
-'''
